bd92f263951af1304f3ad52fa702ae4a0f598508
tiffanylyx <tiffanylyx@foxmail.com>
2022-07-22 13:14:04 +0800

    update

--- generate_quad2.py
+++ generate_quad2.py
@@ -26,11 +26,13 @@ from sympy import *
 import random
 
 from utils import *
+from panda3d.core import PythonTask
 
+import time
 loadPrcFileData('', 'win-size 1200 600')
 
 base = ShowBase()
-
+previous_time = time.time()
 # You can't normalize inline so this is a helper function
 def normalized(*args):
     myVec = LVector3(*args)
@@ -89,12 +91,45 @@ y_old_1 = -10
 z_old_1 = 0
 
 snode = GeomNode('square')
-
+'''
 alight = AmbientLight('alight')
 alight.setColor((0.9, 0.9, 0.9, 0.4))
+#alight.setShadowCaster(True, 512, 512)
 alnp = render.attachNewNode(alight)
 render.setLight(alnp)
 render.setAntialias(AntialiasAttrib.MAuto)
+'''
+
+'''
+plight = PointLight('plight')
+plight.setColor((0.8, 0.8, 0.8, 1))
+plight.setShadowCaster(True, 512, 512)
+plnp = render.attachNewNode(plight)
+plnp.setPos(0, 0, 0)
+render.setLight(plnp)
+
+plight = PointLight('plight')
+plight.setColor((0.8, 0.8, 0.8, 1))
+plight.setShadowCaster(True, 512, 512)
+plnp = render.attachNewNode(plight)
+plnp.setPos(10, 0, 0)
+render.setLight(plnp)
+'''
+plight = PointLight('plight')
+plight.setColor((0.8, 0.8, 0.8, 1))
+plight.setShadowCaster(True, 512, 512)
+plnp = render.attachNewNode(plight)
+plnp.setPos(0, 10, 0)
+render.setLight(plnp)
+
+plight = PointLight('plight')
+plight.setColor((0.8, 0.8, 0.8, 1))
+plight.setShadowCaster(True, 512, 512)
+plnp = render.attachNewNode(plight)
+plnp.setPos(0, 0, 10)
+render.setLight(plnp)
+
+render.setShaderAuto()
 
 angleDegrees = 0
 angleDegreesZ = 0
@@ -103,17 +138,21 @@ angle1 = 0
 
 #callback function to set  text
 def setText(sentence):
+    global x_old_1,y_old_1,z_old_1snode,angle1, previous_time
+    now_time = time.time()
+    time_period = now_time-previous_time
     sentence = sentence.lower()
-    global x_old_1,y_old_1,z_old_1snode,angle1
+
     textObject.setText(sentence)
     if sentence[-1]==".":
         sentence = sentence[:-1]
-    x_old_1 = x_old_1+10
+    x_old_1 = x_old_1+time_period*0.8
+    previous_time = now_time
     y_old_1 = -10
     z_old_1 = 0
 
-
     sent_vect = compute_sent_vec(sentence, model_sentence)
+    [x_old_2, y_old_2, z_old_2] = solve_point_on_vector(x_old_1, y_old_1, z_old_1, 2, sent_vect[0],sent_vect[1], sent_vect[2]) # spread along the sentence vec
 
     word_list = sentence.split(" ")
 
@@ -121,18 +160,16 @@ def setText(sentence):
     count_2 = 0
     for word in word_list:
         random.shuffle(sent_vect)
-        distance = 0.5 #word speak time
         w = compute_word_length(word)/2
-        l = 2. # how to decide?
+        #l = 2. # how to decide?
         i0 = random.choice((-1, 1))
         i1 = random.choice((-1, 1))
         i2 = random.choice((-1, 1))
 
         [x1, y1, z1] = solve_point_on_vector(x_old_1, y_old_1, z_old_1, w, i0*sent_vect[0],i1*sent_vect[1], i2*sent_vect[2]) # spread along the sentence vec
 
-        print(x1, y1, z1)
         angle1 = atan(y1/x1)*180
-        print("angle1",angle1)
+
         if res_parts[count_2][1] == 'VERB':
             dim = 2
         else: dim = 3
@@ -141,7 +178,7 @@ def setText(sentence):
         [x2, y2, z2] = solve_point_on_vector(x1, y1, z1, w, nx, ny, nz)
 
         color_value = compute_word_vec(word, model, pca2, pca3, pca4, 4) #word vector 4D
-
+        '''
         if count_2==0:
             x_old_1 = x1
             y_old_1 = y1
@@ -152,40 +189,36 @@ def setText(sentence):
             z_old_2 = z2
 
         else:
-            square = makeQuad(x_old_1, y_old_1, z_old_1, x_old_2, y_old_2, z_old_2, x1, y1, z1, x2, y2, z2, color_value)
-            x_old_1 = x1
-            y_old_1 = y1
-            z_old_1 = z1
+            '''
+        square = makeQuad(x_old_1, y_old_1, z_old_1, x_old_2, y_old_2, z_old_2, x1, y1, z1, x2, y2, z2, color_value)
+        x_old_1 = x1
+        y_old_1 = y1
+        z_old_1 = z1
 
-            x_old_2 = x2
-            y_old_2 = y2
-            z_old_2 = z2
+        x_old_2 = x2
+        y_old_2 = y2
+        z_old_2 = z2
 
-            snode.addGeom(square)
+        snode.addGeom(square)
         count_2+=1
 
     cube = render.attachNewNode(snode)
     cube.setDepthWrite(True, 100)
-
-
     cube.setTwoSided(True)
 
 
-    print("pos",base.camera.getHpr())
-
-    print("pos",base.camera.getPos())
 
 #clear the text
 def clearText():
     entry.enterText('')
-    
+
 bk_text = "This is my Demo"
 textObject = OnscreenText(text=bk_text, pos=(1.5, -0.95), scale=0.05,
                           fg=(1, 0.5, 0.5, 1), align=TextNode.ACenter,
                           mayChange=1)
 #add text entry
 entry = DirectEntry(text = "", scale=.05, command=setText,pos=(1.25, 0,-0.75),
-initialText="Type Something", numLines = 2, focus=1, focusInCommand=clearText)
+initialText="Type Something", numLines = 3, focus=1, focusInCommand=clearText)
 
 
 
@@ -194,7 +227,8 @@ def spinCameraTask(task):
     angleDegrees = task.time * 6.0
     angleRadians = angleDegrees * (pi / 180.0)
 
-    base.camera.setPos(0,y_old_1+10,0)
+
+    base.camera.setY(y_old_1+10)
     base.camera.lookAt(x_old_1,y_old_1,z_old_1)
     return Task.cont
 
